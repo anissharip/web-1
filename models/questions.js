3fd0c92bab5e2def4fc446366360fa9de25e96b6
anissharip <anis.sharip@gmail.com>
2014-12-09 17:41:25 +0800

    Update questions.js

--- models/questions.js
+++ models/questions.js
@@ -34,6 +34,6 @@ module.exports = function (orm, db) {
                 cache: false
             });
     question.sync(function (err) {
-        err && console.log(err);
+        console.log(err);
     });
 };
