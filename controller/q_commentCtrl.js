99606539b2275c64a11b4914c3ca30440ff50195
anissharip <anis.sharip@gmail.com>
2014-12-09 12:08:36 +0800

    Update q_commentCtrl.js

--- controller/q_commentCtrl.js
+++ controller/q_commentCtrl.js
@@ -1,95 +1,102 @@
-var orm=require('orm');
+/*jslint nomen: true */
+/*jslint unparam: true*/
+/*jslint node: true */
+"use strict";
+
+var orm = require('orm');
 
 //get all comments of a question
 exports.getAllQuestionComments = function (req, res) {
 
     req.models.q_comment.find(
-    {
-        'question_id': req.params.question_id
-    },function (err, qcomments) {
-        if(err){
-            res.status(404).send("Can not find comments of this question");
-        }else{
-             res.format({
-                    'application/json': function(){
+        {
+            'question_id': req.params.question_id
+        },
+        function (err, qcomments) {
+            if (err) {
+                res.status(404).send("Can not find comments of this question");
+            } else {
+                res.format({
+                    'application/json': function () {
                         res.status(200).send(qcomments);
                     },
-                    'text/html': function(){
+                    'text/html': function () {
                        // res.redirect('/questions/');
-                       res.status(200).send("All question comments: " + JSON.stringify(qcomments));
+                        res.status(200).send("All question comments: " + JSON.stringify(qcomments));
                     },
-                    'default': function(){
+                    'default': function () {
                         res.status(406).send("Not Acceptable");
                     }
                 });
-            console.log("get all comments of this question successfully!");
+                console.log("get all comments of this question successfully!");
+            }
         }
-    });
+    );
 };
 
 //get a comment of a question
 exports.getQuestionComment = function (req, res) {
 
     req.models.q_comment.get(req.params.qcomment_id, function (err, qcomment) {
-        if(err){
+        if (err) {
             res.status(404).send('Not found the question comment');
-        }else {
-            if(qcomment.question_id!=req.params.question_id){
+        } else {
+            if (qcomment.question_id !== req.params.question_id) {
                 res.send('the comment does not belong to this question');
-            }else{
+            } else {
                 res.format({
-                    'application/json': function(){
+                    'application/json': function () {
                         res.status(200).send(qcomment);
                     },
-                    'text/html': function(){
+                    'text/html': function () {
                       // res.redirect('/questions/' + req.params.question_id);
-                      res.status(200).send("the question comment is " + JSON.stringify(qcomment));
+                        res.status(200).send("the question comment is " + JSON.stringify(qcomment));
                     },
-                    'default': function(){
+                    'default': function () {
                         res.status(406).send("Not Acceptable");
                     }
                 });
                 console.log('get a question comment  successfully');
             }
-        } 
+        }
     });
 };
 //create a question comment of a question
-exports.createQuestionComment = function(req,res){
-    var question_id=req.params.question_id;
+exports.createQuestionComment = function (req, res) {
+    var question_id = req.params.question_id;
 
     req.models.q_comment.create({
-        contents:req.body.contents,
-        createTime:Date.now,
-        question_id:question_id
-    },function(err,qcomment){
-        if(err) {
+        contents: req.body.contents,
+        createTime: Date.now,
+        question_id: question_id
+    }, function (err, qcomment) {
+        if (err) {
             res.status(500).send("Internal Server Error");
-        }else{
+        } else {
             res.format({
-                'application/json': function(){
+                'application/json': function () {
                     res.status(200).send(qcomment);
                 },
-                'text/html': function(){
+                'text/html': function () {
                     res.redirect('/questions/' + req.params.question_id);
                 },
-                'default': function(){
+                'default': function () {
                     res.status(406).send("Not Acceptable");
                 }
             });
             console.log('created a question comment successfully');
         }
-    })
+    });
 }
 //update a question comment via question comment id
-exports.updateQuestionComment = function(req,res){
-    req.models.q_comment.get(req.params.qcomment_id,function(err,qcomment){
-        if(err) { 
+exports.updateQuestionComment = function (req,res) {
+    req.models.q_comment.get(req.params.qcomment_id,function (err, qcomment) {
+        if (err) { 
             res.status(404).send("Can not find the question comment");
-        }else{
-            if(!req.body.contents){
+        } else {
+            if (!req.body.contents) {
                 res.status(400).send('Bad request');
-            }else{
+            } else {
                 qcomment.contents=req.body.contents;
             };
 
